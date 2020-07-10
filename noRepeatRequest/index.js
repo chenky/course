81f439cb7a1cbc7b0f7f407390177622f3638ae0
chenqiliang <100954121@hdjt.hdad.local>
2020-07-10 15:22:29 +0800

    axios中防止重复提交函数

--- noRepeatRequest/index.js
+++ noRepeatRequest/index.js
@@ -1,3 +1,5 @@
+// 防止用户在一个请求没有响应回客户端，就去重复发请求
+
 // 如果使用jquery类库的ajax，则添加一个serialAjax
 // 同时支持promise和普通回掉函数的写法，防止重复，防重
 // 同一个请求只有当前请求返回后，才允许用户再次请求，但不同请求不受影响可以并行发送
@@ -22,25 +24,30 @@
 
 // 如果使用的是axios类库一样可以封装函数
 // 防重，防止重复提交axios
-const ajax = {
-  // 防止重复提交
-  serialRequest: function (config, param) {
-    config.param = param
-    if (!config.url) {
-      throw new Error('提交的url不能为空')
-    }
-    if (this.serialRequest[config.url]) {
-      // 正在提交中
-      // return new Promise((resolve,reject)=>{})
-      console.log('请求正在提交，直接返回')
-      return
-    }
-    this.serialRequest[config.url] = true
-    // let self = this
-    return axios.request(config).finally(() => {
-      // 请求结束重置请求，以允许用户再次提交
-      console.log('数据已经返回，重置请求状态标识符')
-      this.serialRequest[config.url] = false
+// 防止重复提交，同一个请求（url+params作为key区分）串行发送请求
+function serialRequest(config) {
+  if (!config.url) {
+    throw new Error('提交的url不能为空')
+  }
+  let guid = config.serialId
+  if (!guid) {
+    let { url, params } = config
+    // 需要使用qs对params对象转换成string
+    params = qs.stringify(params)
+    guid = url + (typeof params === 'string' ? params : '')
+  }
+  if (this.serialRequest[guid]) {
+    // 当前请求正在提交中，待请求返回后方可再次提交
+    // console.log('当前请求正在提交中，待请求返回后方可再次提交')
+    return new Promise((resolve, reject) => {
+      resolve({ code: 'serial-request', msg: '当前请求正在提交中，待请求返回后方可再次提交' })
     })
   }
+  this.serialRequest[guid] = true
+  // let self = this
+  return axios.request(config).finally(() => {
+    // 请求结束重置请求，以允许用户再次提交,重置为null防止内存泄露
+    // console.log('数据已经返回，重置请求状态标识符')
+    this.serialRequest[guid] = null
+  })
 }

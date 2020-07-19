17126ca4c2ccd9efaa02034f41eecfe9266056af
谌启梁 <cql@chenqideMacBook-Pro.local>
2020-07-19 18:42:10 +0800

    promise异步编程和模块化导入导出

--- EcmaScript/12.模块化封装代码/src/js/component/module1.js
+++ EcmaScript/12.模块化封装代码/src/js/component/module1.js
@@ -8,14 +8,31 @@
 export let thisObj = this;
 
 export var color = "red";
-export let name = "Nicholas";
+export let name = "cql";
 export const magicNumber = 7;
 
-// export function
+// export function, 如果是默认模块，导入的时候不用{}
 // export default function sum(num1, num2) {
-    export function sum(num1, num2) {
+export function sum(num1, num2) {
     return num1 + num2;
 }
+
+// 导出默认值
+// 每个模块只能有一个默认导出值
+// 1.
+export default function(num1, num2) {
+    return num1 + num2;
+}
+// // 2.
+function sum(num1, num2) {
+    return num1 + num2;
+}
+export default sum;
+// // 3.
+function sum(num1, num2) {
+    return num1 + num2;
+}
+export { sum as default };
 
 // export class
 export class Rectangle {
@@ -34,6 +51,36 @@ function subtract(num1, num2) {
 function multiply(num1, num2) {
     return num1 * num2;
 }
-
 // ...and then export it later
 export { multiply };
+// 导入导出重命名
+function sum(num1, num2) {
+    return num1 + num2;
+}
+export { sum as add };
+
+
+
+
+// 重新导出绑定
+import { sum } from "./example.js";
+export { sum }
+// // 或者
+export { sum } from "./example.js";
+
+export { sum as add } from "./example.js";
+
+// // example如果有默认导出，无法定义一个新的默认导出
+export * from "./example.js";
+
+// module code without exports or imports
+Array.prototype.pushAll = function(items) {
+
+  // items must be an array
+  if (!Array.isArray(items)) {
+      throw new TypeError("Argument must be an array.");
+  }
+
+  // use built-in push() and spread operator
+  return this.push(...items);
+};
--- EcmaScript/12.模块化封装代码/src/js/pages/page1.js
+++ EcmaScript/12.模块化封装代码/src/js/pages/page1.js
@@ -1,34 +1,44 @@
 
 
         // 导入单个模块
-        // import { color } from "../component/module1";
-        // console.log(color);
+        import { color } from "../component/module1";
+        console.log(color);
 
         // 导入多个模块
-        // import {thisObj, color,name,magicNumber,sum,Rectangle,multiply} from "../component/module1";
-        // console.log(color);
-        // console.log(name);
-        // console.log(magicNumber);
-        // console.log(sum(1,2));
-        // console.log(Rectangle);
-        // console.log(multiply(2,2));
-        // console.log("this is page1");        
-        // console.log(thisObj);
+        import {thisObj, color,name,magicNumber,sum,Rectangle,multiply} from "../component/module1";
+        console.log(color);
+        console.log(name);
+        console.log(magicNumber);
+        console.log(sum(1,2));
+        console.log(Rectangle);
+        console.log(multiply(2,2));
+        console.log("this is page1");        
+        console.log(thisObj);
 
         // 导入所有模块
-        // import * as test from "../component/module1";
-        // console.log(test.color);
-        // console.log(test.name);
+        import * as test from "../component/module1";
+        console.log(test.color);
+        console.log(test.name);
 
+        // 导入默认值
+        import sum from "../component/module2";
+        console.log(sum(1,3));
+        import add from "../component/module2";
+        console.log(add(1,3));
+
+
+        // 不需要大括号，默认放在非默认之前
+        import sum,{color} from "../component/module2";
+        console.log(sum(1,2));
+        console.log(color);
 
         // 不能动态的导入导出绑定，便于编辑器或工具分析
-
-        // if(flag){
-        //         export flag;
-        // }
-        // function tryImport(){
-        //         import flag from "";
-        // }
+        if(flag){
+                export flag;
+        }
+        function tryImport(){
+                import flag from "";
+        }
         
         
 
